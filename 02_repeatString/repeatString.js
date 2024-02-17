989236dd3f260439c3b2ebd28b8cae4a5274914a
Kabir Ghai <74025064+CypherGuy@users.noreply.github.com>
2024-02-17 22:41:39 +0000

    Complete task 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(toRepeat, times) {
+    if (times < 0) {
+        return 'ERROR';
+    } else if (times === 0) {
+        return '';
+    } else {
+    return toRepeat.repeat(times);
+    }
 };
 
 // Do not edit below this line
