a027a54d659de3c28eb2145ff413e6bac05c8347
Kabir Ghai <74025064+CypherGuy@users.noreply.github.com>
2024-02-18 18:42:16 +0000

    Finished task 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(peopleList) {
+    function getAge(death, birth){
+        if (!death){
+            death = new Date().getFullYear()
+        }
+        return death - birth
+    }
+    return peopleList.reduce((oldestPerson, currentPerson) => {
+        oldAge = getAge(oldestPerson["yearOfDeath"], oldestPerson["yearOfBirth"])
+        newAge = getAge(currentPerson["yearOfDeath"], currentPerson["yearOfBirth"])
+        if (oldAge >= newAge) {
+            return oldestPerson;
+        } else {
+            return currentPerson;
+        }
+    })
 };
 
 // Do not edit below this line
