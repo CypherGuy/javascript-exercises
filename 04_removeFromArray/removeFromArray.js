4638fce7efd258d3ce2ae041febc46e205c308ba
Kabir Ghai <74025064+CypherGuy@users.noreply.github.com>
2024-02-18 15:13:57 +0000

    Completed task 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arrayOfItems, ...itemsToRemove) {
+    items = Array.from(arrayOfItems)
+    for (let i=0; i < items.length; i++){
+        for (let j=0; j < arrayOfItems.length; j++){
+        if (arrayOfItems[i] == itemsToRemove[j]){
+            arrayOfItems.splice(i, 1)
+            i = 0
+            j = 0
+    }
+        }
+    }
+    return arrayOfItems
 };
 
 // Do not edit below this line
