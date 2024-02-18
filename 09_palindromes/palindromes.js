c49243e397bba2b1c5d66bc2e97c7ddb4baab2b0
Kabir Ghai <74025064+CypherGuy@users.noreply.github.com>
2024-02-18 17:59:02 +0000

    Complete task 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
-
-};
+function palindromes(thePalindrome) {
+    let splittedPalindrome = thePalindrome.split("") 
+    const word = splittedPalindrome.filter((letter) => ((letter >= "a" && letter <= "z" || letter >= "A" && letter <= "Z")) || ('0' <= letter && letter <= '9')).map(v => v.toLowerCase())
+    for(let i = 0; i < word.length; i++){
+      if(word[i] === word[word.length - 1 - i]){
+        continue;
+      } else {
+        return false;
+      }
+    }
+    return true
+   };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+//  r a c e c a r
