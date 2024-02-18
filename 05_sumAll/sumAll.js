7774ad2a15d2c060809f4f9acc5cbcbb017ca685
Kabir Ghai <74025064+CypherGuy@users.noreply.github.com>
2024-02-18 15:39:04 +0000

    Complete task 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let counter = 0
+    if (typeof start !== 'number' || typeof end !== 'number' || start < 0 || end < 0) {
+        return 'ERROR'
+    }
+    if (start > end) {
+        let temp = start
+        start = end
+        end = temp
+    }
+    
+    for(let i = start; i <= end; i++) {
+        counter = counter + i
+    }
 
+    return counter
 };
 
 // Do not edit below this line
