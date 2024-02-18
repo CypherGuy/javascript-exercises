a0fa36caf2fbcd98bcd37aff086d39cc38616809
Kabir Ghai <74025064+CypherGuy@users.noreply.github.com>
2024-02-18 18:11:37 +0000

    Complete task 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,21 @@
-const fibonacci = function() {
+/* I'm going to use Recursion.
 
+It needs 3 things:
+
+- Base Case
+- A "breakout"
+- A call to itself
+*/
+const fibonacci = function(number) {
+    number = parseInt(number); //Convert string to number
+    if (number < 0) {
+        return "OOPS";
+    } 
+    if (number < 2) { 
+        return number; // Base Case
+    } else {
+        return fibonacci(number - 1) + fibonacci(number - 2); //This is the recursion  and the "breakout" bit.
+    }
 };
 
 // Do not edit below this line
