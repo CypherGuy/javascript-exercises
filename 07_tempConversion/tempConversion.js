d79a608b6a5371ea154c9ea6b646f86b4a55defa
Kabir Ghai <74025064+CypherGuy@users.noreply.github.com>
2024-02-18 17:15:46 +0000

    Complete task 6

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreeFahrenheit) {
+  let c = (degreeFahrenheit - 32) * 5 / 9;
+  return Math.round(c * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreeCelcius) {
+  let f = degreeCelcius * 9 / 5 + 32;
+  return Math.round(f * 10) / 10;
 };
 
 // Do not edit below this line
