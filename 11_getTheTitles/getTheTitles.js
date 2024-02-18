143cf453bdaddad4dbb8e5df6144ba4a84d61165
Kabir Ghai <74025064+CypherGuy@users.noreply.github.com>
2024-02-18 18:18:24 +0000

    Complete task 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(bookDict) {
+    titles = []
+    for(let i = 0; i < bookDict.length; i++){
+        titles.push(bookDict[i]["title"])
+    }
+    return titles
 };
 
 // Do not edit below this line
