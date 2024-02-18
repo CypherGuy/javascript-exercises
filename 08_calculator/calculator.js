b265641f0afb2f1066f0c20100eec0173d5e7459
Kabir Ghai <74025064+CypherGuy@users.noreply.github.com>
2024-02-18 17:34:43 +0000

    Complete task 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,39 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;
 };
 
-const subtract = function() {
-	
-};
+const subtract = function(num1, num2) {
+  return num1 - num2;
+}
 
 const sum = function() {
-	
+  let result = 0;
+  for (let i = 0; i <= num.length - 1; i++) {
+      result += num[i];
+  }
+  return result
+  
 };
 
-const multiply = function() {
-
+const multiply = function(...args) {
+  let result = 1;
+  for (let i = 0; i < numbers.length; i++) {
+      result *= numbers[i];
+  }
+  return result
 };
 
-const power = function() {
-	
+const power = function(num, raisedTo) {
+	return Math.pow(num, raisedTo);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let fac = num
+  for(let i = num - 1; i >= 1; i--){
+    fac = fac * i
+  }
+  return fac
+  
 };
 
 // Do not edit below this line
